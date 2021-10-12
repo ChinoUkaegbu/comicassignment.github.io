17b5f3b622269c1e2fb1ccbd9926b0eb9ad3c826
ChinoUkaegbu <chinoukaegbu@gmail.com>
2021-10-12 16:29:18 +0400

    first push

---
+++ animation.js
@@ -0,0 +1,20 @@
+//
+// let Luggage = document.getElementById("luggage")
+// console.log(Luggage);
+// Luggage.addEventListener("click",()=>{
+//   console.log("success!");
+// })
+
+// clicking the safety-check >> pop up dialogue box
+let SafetyCheck = document.getElementById("safetyCheck")
+console.log(SafetyCheck);
+SafetyCheck.addEventListener("click",()=>{
+  document.getElementById("textbox").innerText = "No...The cat won't pass the safety-check itself right? It must be hidden somewhere..."
+})
+
+// clicking the luggage
+// let SafetyCheck = document.getElementById("safetyCheck")
+// console.log(SafetyCheck);
+// SafetyCheck.addEventListener("click",()=>{
+//   document.getElementById("textbox").innerText = "You have clicked on a star!"
+// })
